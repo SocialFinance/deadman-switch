948a612530f34c76ae9ad8c94a49aed37a65c99c
Dave Pederson <dpederson@sofi.org>
2017-11-22 12:23:48 -0700

    Start aggregate task views

---
+++ ui/app/controllers/TasksController.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import org.sofi.deadman.client._
+import javax.inject.Inject
+import play.api.mvc.{ AbstractController, ControllerComponents }
+
+final class TasksController @Inject() (cc: ControllerComponents)(implicit sys: ActorSystem) extends AbstractController(cc) {
+
+  implicit val ec = cc.executionContext
+  implicit val am = ActorMaterializer()
+  val client = Client()
+
+  private def runQuery(id: String, label: String, query: Query) =
+    client.tasks(query).map { t ⇒
+      Ok(views.html.tasks(id, label, t))
+    }
+
+  def tasks(id: String) = Action.async {
+    runQuery(id, "Active Tasks", Query(id, Query.Aggregate))
+  }
+
+  def expirations(id: String) = Action.async {
+    runQuery(id, "Expired Tasks", Query(id, Query.Aggregate, Query.Expired))
+  }
+
+  def warnings(id: String) = Action.async {
+    runQuery(id, "Task Warnings", Query(id, Query.Aggregate, Query.Warning))
+  }
+}
