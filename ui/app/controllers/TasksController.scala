d09fd16b0fa0cf77c2037a81128fd32f95a87959
Dave Pederson <dpederson@sofi.org>
2017-11-26 17:27:14 -0700

    Make tasks controller a singleton

--- ui/app/controllers/TasksController.scala
+++ ui/app/controllers/TasksController.scala
@@ -3,18 +3,19 @@ package controllers
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import org.sofi.deadman.client._
-import javax.inject.Inject
+import javax.inject.{ Inject, Singleton }
 import play.api.mvc.{ AbstractController, ControllerComponents }
 
-final class TasksController @Inject() (cc: ControllerComponents)(implicit sys: ActorSystem) extends AbstractController(cc) {
+@Singleton
+final class TasksController @Inject() (cc: ControllerComponents)(implicit system: ActorSystem) extends AbstractController(cc) {
 
-  implicit val ec = cc.executionContext
-  implicit val am = ActorMaterializer()
+  implicit val executionContext = cc.executionContext
+  implicit val materializer = ActorMaterializer()
   implicit val client = Client()
 
   private def runQuery(id: String, label: String, query: Query) =
-    query.exec().map { t ⇒
-      Ok(views.html.tasks(id, label, t))
+    query.exec().map { tasks ⇒
+      Ok(views.html.tasks(id, label, tasks))
     }
 
   def tasks(id: String) = Action.async {
